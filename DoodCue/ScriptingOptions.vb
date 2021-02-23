bded4237ab3ee672ed794588ecad83f40453f2f3
Coopydood <39441479+Coopydood@users.noreply.github.com>
2021-02-23 21:13:05 +0000

    Initial2

---
+++ DoodCue/ScriptingOptions.vb
@@ -0,0 +1,8 @@
+﻿Public Class ScriptingOptions
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        StopWarning.Show()
+        Me.Hide()
+
+
+    End Sub
+End Class
